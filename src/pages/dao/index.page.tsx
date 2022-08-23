ea05459d39fb9f09e06aa96f95666d53c4cee41b
AndreyBS1 <andrey.babnekov.00@gmail.com>
2022-08-23 17:38:51 +0300

    feat: create project template

---
+++ src/pages/dao/index.page.tsx
@@ -0,0 +1,3 @@
+export default function DaoPage() {
+  return <div>DaoPage</div>
+}
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,11 +1,18 @@
+import { Container, TextField } from '@mui/material'
 import { Header } from '@shared/ui'
+import { Footer } from '@shared/ui/footer'
 
-export interface IHomePageProperties {}
-
-export default function HomePage(props: IHomePageProperties) {
+export default function HomePage() {
   return (
     <>
       <Header>Header</Header>
+
+      <Container>
+        <TextField label="DAO name" variant="outlined" />
+        <TextField multiline label="DAO description" variant="outlined" />
+      </Container>
+
+      <Footer>Footer</Footer>
     </>
   )
 }
