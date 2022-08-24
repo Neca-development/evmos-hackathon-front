7ab078184fb8a3010adcb80fdc7c6da11e1740b0
AndreyBS1 <andrey.babnekov.00@gmail.com>
2022-08-24 20:32:55 +0300

    feat: add dao entity api

---
+++ src/entities/dao/dao.api.ts
@@ -0,0 +1,37 @@
+import { createApi } from '@reduxjs/toolkit/dist/query'
+import { baseQuery } from '@shared/api'
+
+export const daoApi = createApi({
+  reducerPath: 'clientsApi',
+  baseQuery,
+  tagTypes: ['DAO'],
+  endpoints: (builder) => ({
+    generateDaoInfoLink: builder.mutation<any, any>({
+      query: (body) => {
+        return {
+          url: '',
+          method: 'POST',
+          body,
+        }
+      },
+      invalidatesTags: ['DAO'],
+      transformResponse: (res: any) => {
+        return res.data
+      },
+    }),
+
+    generateImageLinks: builder.mutation<any, any>({
+      query: (body) => {
+        return {
+          url: '',
+          method: 'POST',
+          body,
+        }
+      },
+      invalidatesTags: ['DAO'],
+      transformResponse: (res: any) => {
+        return res.data
+      },
+    }),
+  }),
+})
