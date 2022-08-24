294eec6531b4106660a6becbd9383ef6dca1ddab
AndreyBS1 <andrey.babnekov.00@gmail.com>
2022-08-24 13:40:44 +0300

    feat: update header

--- src/shared/ui/header/header.component.tsx
+++ src/shared/ui/header/header.component.tsx
@@ -1,16 +1,38 @@
-import classNames from 'classnames'
+import { Avatar, Button } from '@mui/material'
+import * as React from 'react'
 
-export interface IHeaderProperties {
-  children: any
-  className?: string
-}
+import { Paragraph } from '../typography'
 
-export function Header(props: IHeaderProperties) {
+export function Header() {
+  const [account, setAccount] = React.useState(false)
+
   return (
-    <header className={classNames('header')}>
-      <div className={classNames('header__content ', props.className)}>
-        {props.children}
-      </div>
+    <header className="h-10 px-3 space-x-5 flex justify-end items-center bg-[#EDEDED]">
+      {account ? (
+        <>
+          <Paragraph>Wallet address</Paragraph>
+          <Avatar className="h-6 w-6 text-[0.65rem] bg-[#333333]">U</Avatar>
+          <Button
+            variant="contained"
+            size="small"
+            className="text-[0.65rem] text-white bg-[#333333] hover:bg-[#4e4e4e]"
+            onClick={() => setAccount(false)}
+          >
+            Disconnect
+          </Button>
+        </>
+      ) : (
+        <>
+          <Button
+            variant="contained"
+            size="small"
+            className="text-[0.65rem] text-white bg-[#333333] hover:bg-[#4e4e4e]"
+            onClick={() => setAccount(true)}
+          >
+            Connect
+          </Button>
+        </>
+      )}
     </header>
   )
 }
