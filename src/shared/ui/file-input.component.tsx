99621e4b41bb241bcfa12a1bb5af971de2af15be
AndreyBS1 <andrey.babnekov.00@gmail.com>
2022-08-24 17:53:00 +0300

    feat: add create dao page

--- src/shared/ui/file-input.component.tsx
+++ src/shared/ui/file-input.component.tsx
@@ -1,35 +1,46 @@
-import { fileToBlob } from '@shared/utils'
+import classNames from 'classnames'
 import * as React from 'react'
 
-import { AttachIcon } from './icons'
-
 export interface IFileInputProperties {
-  onChange: Function
+  size?: 'small' | 'large'
+  inputName: string
+  imgFile: File | null | undefined
+  onImageChange: (name: string, newImage: File | null | undefined) => void
 }
 
-export function FileInput({ onChange }: IFileInputProperties) {
-  const handleFileInputChange = async (el: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(el)
-    if (el.target.files) {
-      const blobArray = await Promise.all(
-        Array.from(el.target.files).map(async (file) => {
-          return fileToBlob(file)
-        })
-      ).then((res) => res)
+export function FileInput(props: IFileInputProperties) {
+  const { inputName, imgFile, size = 'small', onImageChange } = props
 
-      onChange(blobArray)
-    }
+  const handleFileInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, files } = event.target
+    if (!files) return
+
+    const newImage = files[0]
+
+    onImageChange(name, newImage)
   }
+
   return (
-    <label>
+    <label
+      className={classNames(
+        'flex justify-center items-center bg-[#D9D9D9] cursor-pointer',
+        size === 'small' && 'h-[7rem] w-[7rem]',
+        size === 'large' && 'h-[20rem] w-[20rem]'
+      )}
+    >
       <input
-        className="hidden"
-        multiple={true}
+        name={inputName}
+        hidden
         type={'file'}
         accept="image/*"
         onChange={handleFileInputChange}
       />
-      <AttachIcon />
+
+      {imgFile ? (
+        <img src={URL.createObjectURL(imgFile)} alt="" className="w-full" />
+      ) : (
+        <img src="/assets/images/add.svg" alt="" className="h-1/6 w-1/6" />
+      )}
     </label>
   )
 }
