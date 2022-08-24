283acf40c59be1f6505a60b6bbbbb96620bd970d
AndreyBS1 <andrey.babnekov.00@gmail.com>
2022-08-24 18:33:32 +0300

    feat: install usedapp

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,14 +1,26 @@
 import '@shared/styles/global.scss'
 
 import { store } from '@shared/store'
+import type { Config } from '@usedapp/core'
+import { DAppProvider, Rinkeby } from '@usedapp/core'
+import { getDefaultProvider } from 'ethers'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 
+const config: Config = {
+  readOnlyChainId: Rinkeby.chainId,
+  readOnlyUrls: {
+    [Rinkeby.chainId]: getDefaultProvider('rinkeby'),
+  },
+}
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <DAppProvider config={config}>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </DAppProvider>
   )
 }
 
--- src/shared/ui/header/header.component.tsx
+++ src/shared/ui/header/header.component.tsx
@@ -1,22 +1,28 @@
 import { Avatar, Button } from '@mui/material'
+import { useEthers } from '@usedapp/core'
 import * as React from 'react'
 
 import { Paragraph } from '../typography'
 
 export function Header() {
-  const [account, setAccount] = React.useState(false)
+  const { account, activateBrowserWallet, deactivate } = useEthers()
+
+  const walletAddress = React.useMemo(
+    () => `${account?.slice(0, 4)}...${account?.slice(-6, account?.length)}`,
+    [account]
+  )
 
   return (
     <header className="h-10 px-3 space-x-5 flex justify-end items-center bg-[#EDEDED]">
       {account ? (
         <>
-          <Paragraph>Wallet address</Paragraph>
+          <Paragraph>{walletAddress}</Paragraph>
           <Avatar className="h-6 w-6 text-[0.65rem] bg-[#333333]">U</Avatar>
           <Button
             variant="contained"
             size="small"
             className="text-[0.65rem] text-white bg-[#333333] hover:bg-[#4e4e4e]"
-            onClick={() => setAccount(false)}
+            onClick={deactivate}
           >
             Disconnect
           </Button>
@@ -27,7 +33,7 @@ export function Header() {
             variant="contained"
             size="small"
             className="text-[0.65rem] text-white bg-[#333333] hover:bg-[#4e4e4e]"
-            onClick={() => setAccount(true)}
+            onClick={activateBrowserWallet}
           >
             Connect
           </Button>
