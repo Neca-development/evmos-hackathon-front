50739fc7aa50f9a2e60f8f97f92a776b05145e33
AndreyBS1 <andrey.babnekov.00@gmail.com>
2022-08-25 17:23:04 +0300

    feat: update header

--- src/shared/ui/header.component.tsx
+++ src/shared/ui/header.component.tsx
@@ -2,11 +2,10 @@ import { Avatar } from '@mui/material'
 import { useEthers } from '@usedapp/core'
 import * as React from 'react'
 
-import { MButton } from './mbutton.component'
 import { Paragraph } from './typography'
 
 export function Header() {
-  const { account, activateBrowserWallet, deactivate } = useEthers()
+  const { account } = useEthers()
 
   const walletAddress = React.useMemo(
     () => `${account?.slice(0, 4)}...${account?.slice(-6, account?.length)}`,
@@ -14,18 +13,13 @@ export function Header() {
   )
 
   return (
-    <header className="h-10 px-3 space-x-5 flex justify-end items-center">
-      {account ? (
+    <header className="h-10 px-[7rem] space-x-3 flex justify-end items-center">
+      {account && (
         <>
           <Paragraph>{walletAddress}</Paragraph>
-          <Avatar className="h-6 w-6 text-[0.65rem] text-gray bg-white">U</Avatar>
-          <MButton variant="secondary" onClick={deactivate}>
-            Disconnect
-          </MButton>
-        </>
-      ) : (
-        <>
-          <MButton onClick={activateBrowserWallet}>Connect</MButton>
+          <Avatar className="h-6 w-6 bg-white">
+            <img src="/assets/images/metamask.svg" alt="" className="w-2/3" />
+          </Avatar>
         </>
       )}
     </header>
