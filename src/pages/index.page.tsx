8019d094b210e2e62925abb03b72624da78c764a
AndreyBS1 <andrey.babnekov.00@gmail.com>
2022-08-29 11:37:52 +0300

    feat: add wallet connection to main page

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,13 +1,29 @@
-import { Header, HeadingOne, MainContainer } from '@shared/ui'
+import { Header, HeadingOne, MainContainer, MButton } from '@shared/ui'
+import { useEthers } from '@usedapp/core'
+import { useRouter } from 'next/router'
+import * as React from 'react'
 
 export default function HomePage() {
+  const { activateBrowserWallet } = useEthers()
+  const router = useRouter()
+
+  React.useEffect(() => {
+    router.prefetch('/profile')
+  }, [])
+
+  const connectWallet = async () => {
+    await activateBrowserWallet()
+    router.push('/profile')
+  }
+
   return (
     <>
       <div className="h-screen flex flex-col">
         <Header />
 
-        <MainContainer className="flex-grow flex justify-center items-center">
+        <MainContainer className="flex-grow space-y-3 flex flex-col justify-center items-center">
           <HeadingOne>Evmos Hackathon</HeadingOne>
+          <MButton onClick={connectWallet}>Connect wallet</MButton>
         </MainContainer>
       </div>
     </>
