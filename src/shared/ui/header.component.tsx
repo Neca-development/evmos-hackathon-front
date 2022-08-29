0c0f1c909cad7d525f49b044d5c650d3122c3a35
AndreyBS1 <andrey.babnekov.00@gmail.com>
2022-08-29 18:14:28 +0300

    feat: add profile link to header

--- src/shared/ui/header.component.tsx
+++ src/shared/ui/header.component.tsx
@@ -1,5 +1,6 @@
 import { Avatar } from '@mui/material'
 import { useEthers } from '@usedapp/core'
+import Link from 'next/link'
 import * as React from 'react'
 
 import { MetamaskIcon } from './icons/metamask.icon'
@@ -12,16 +13,17 @@ export function Header() {
     () => `${account?.slice(0, 4)}...${account?.slice(-6, account?.length)}`,
     [account]
   )
-
   return (
-    <header className="h-10 px-[7rem] space-x-3 flex justify-end items-center">
+    <header className="h-10 px-[7rem] flex justify-end items-center">
       {account && (
-        <>
-          <Paragraph>{walletAddress}</Paragraph>
-          <Avatar className="h-6 w-6 bg-white">
-            <MetamaskIcon className="w-2/3" />
-          </Avatar>
-        </>
+        <Link href="/profile">
+          <a className="space-x-3 flex items-center text-white">
+            <Paragraph>{walletAddress}</Paragraph>
+            <Avatar className="h-6 w-6 bg-white">
+              <MetamaskIcon className="w-2/3" />
+            </Avatar>
+          </a>
+        </Link>
       )}
     </header>
   )
