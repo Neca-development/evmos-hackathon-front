2fbcab96f27762d3351870b23da5ed2ab7a7687a
AndreyBS1 <andrey.babnekov.00@gmail.com>
2022-09-01 16:09:19 +0300

    feat: update dao factory address

--- src/blockchain/constants.ts
+++ src/blockchain/constants.ts
@@ -1,4 +1,4 @@
 export const EVMOS_TESTNET_CHAINID = 9000
 export const EVMOS_TESTNET_RPC_URL = 'https://eth.bd.evmos.dev:8545'
 
-export const DAO_FACTORY_ADDRESS = '0xf5C34E70a18AbEfb88D5ce8aB5Fc4c194e3e56Ff'
+export const DAO_FACTORY_ADDRESS = '0x195672A79f1971571755abAdbc127Ea31FaA852B'
