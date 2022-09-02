d0312a57b8c7150fe48c50bbd7d6f5e3b10ef190
AndreyBS1 <andrey.babnekov.00@gmail.com>
2022-09-02 14:26:31 +0300

    feat: update home page

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,7 +1,10 @@
-import { Header, HeadingOne, MainContainer, MButton } from '@shared/ui'
+import { HeadingOne, MButton } from '@shared/ui'
 import { useEthers } from '@usedapp/core'
+import classNames from 'classnames'
 import { useRouter } from 'next/router'
 import * as React from 'react'
+
+import styles from './index.module.scss'
 
 export default function HomePage() {
   const { activateBrowserWallet } = useEthers()
@@ -17,15 +20,34 @@ export default function HomePage() {
   }
 
   return (
-    <>
-      <div className="h-screen flex flex-col">
-        <Header />
+    <main
+      className={classNames(
+        'h-screen w-full px-14',
+        'flex flex-col justify-center',
+        'bg-homepage bg-cover bg-center bg-no-repeat'
+      )}
+    >
+      <div className="w-[30rem]">
+        <HeadingOne
+          className={classNames(
+            'mb-6',
+            'font-red-hat-display text-5xl leading-snug uppercase text-transparent',
+            styles.title
+          )}
+        >
+          BECOME THE PART OF VOTERS POWER
+        </HeadingOne>
 
-        <MainContainer className="flex-grow space-y-3 flex flex-col justify-center items-center">
-          <HeadingOne>Evmos Hackathon</HeadingOne>
-          <MButton onClick={connectWallet}>Connect wallet</MButton>
-        </MainContainer>
+        <p className="w-[20rem] mb-12 text-xs">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+          nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+
+        <div>
+          <MButton onClick={connectWallet}>Connect Metamask</MButton>
+        </div>
       </div>
-    </>
+    </main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily:{
+        'red-hat-display': ['RedHatDisplay-Regular', '-apple-system', 'BlinkMacSystemFont',
+        '"Segoe UI"', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', '"Open Sans"', '"Helvetica Neue"',
+        'sans-serif']
+      },
       backgroundImage:{
+        'homepage': "url('/assets/images/homepage.svg')",
         'dao-hero': 'linear-gradient(to bottom, #262747 0%, #262747 45%, #1C1D34 45%, #1C1D34 100%)',
         'profile-hero': 'linear-gradient(to bottom, #262747 0%, #262747 56%, transparent 56%, transparent 100%)',
       },
